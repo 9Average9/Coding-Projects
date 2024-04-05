53eb65740c199af45870055ec08ed0a85d02fc66
9Average9 <132110422+9Average9@users.noreply.github.com>
2024-04-04 20:52:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -13,24 +13,9 @@ function clearInput(){
 function calculate(){
   let userInput = document.getElementById('input');
   let input = userInput.value.trim();
+  var checkInput = /^[0-9]+([+\-*/][0-9]+)?$/;
 
 
 
-if(checkInput.test(input)){
-  let result = eval(input);
-userInput.value = result;
-try{
-if(result != undefined && isFinite(result)){
-userInput = result;
-}
-
-
-}catch(error){
-input = 'enter a valid expression'
-}
-
-}else{
-  userInput.value = 'enter a valid expression';
-}
 
 }
