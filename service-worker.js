520378eb34041f6479d289ede884ff73c78f22f3
9Average9 <132110422+9Average9@users.noreply.github.com>
2026-05-07 13:48:06 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "basic-greek-trainer-v1.0.0";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css?v=25",
+  "./vocab.js?v=25",
+  "./app.js?v=25",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
